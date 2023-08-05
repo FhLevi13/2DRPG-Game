ff896ec3af44e61184cffe7f260c8c59eed4fb07
Alep <alifvahlephi46@gmail.com>
2023-08-05 23:36:08 +0700

    player movement

--- Assets/PlayerControll.cs
+++ Assets/PlayerControll.cs
@@ -5,8 +5,13 @@ using UnityEngine.InputSystem;
 
 public class PlayerControll : MonoBehaviour
 {
+    public float moveSpeed = 1f;
+    public float collisionOffset = 0.05f;
+    public ContactFilter2D movementFilter;
+
     Vector2 movementInput;
     Rigidbody2D rb;
+    List<RaycastHit2D> castCollision = new List<RaycastHit2D>();
 
     // Start is called before the first frame update
     void Start() {
@@ -14,13 +19,19 @@ public class PlayerControll : MonoBehaviour
     }
 
     private void FixedUpdate() {
+        // jika movement input bukan 0, mencoba untuk bergerak
         if (movementInput != Vector2.zero) {
-            int count = rb.Cast(
-                movementInput,
-                movementFilter,
-                castCollision,
-                moveSpeed * Time.fixedDeltaTime + collisionOffset
+            // periksa potensi tabrakan
+            int count = rb.Cast (
+                movementInput,  // Nilai X dan Y antara -1 dan 1 yang merepresentasikan arah dari objek yang akan dicari tabrakannya.
+                movementFilter, // pengaturan yang menentukan di mana tabrakan dapat terjadi seperti layer yang akan ditabrakkan.
+                castCollision,  // daftar tabrakan untuk menyimpan tabrakan yang ditemukan setelah Cast selesai.
+                moveSpeed * Time.fixedDeltaTime + collisionOffset   // jumlah yang akan di-cast sama dengan gerakan ditambah offset
             );
+
+            if (count == 0) {
+                rb.MovePosition(rb.position + movementInput * moveSpeed * Time.fixedDeltaTime);
+            }
         }
     }
 
