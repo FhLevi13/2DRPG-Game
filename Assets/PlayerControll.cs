262814e45462334c58e99d61f6c43664e1234ae7
Alep <alifvahlephi46@gmail.com>
2023-08-05 21:03:57 +0700

    up

--- Assets/PlayerControll.cs
+++ Assets/PlayerControll.cs
@@ -1,19 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerControll : MonoBehaviour
 {
     Vector2 movementInput;
+    Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start() {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     private void FixedUpdate() {
-        if (movementInput != Vector2.zero) {}
-        {
-            
+        if (movementInput != Vector2.zero) {
+            int count = rb.Cast(
+                movementInput,
+                movementFilter,
+                castCollision,
+                moveSpeed * Time.fixedDeltaTime + collisionOffset
+            );
         }
     }
 
